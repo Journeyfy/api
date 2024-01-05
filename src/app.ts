d9714c4bf4b39ddb3b669b10e09f322f9a796a30
Alessandro Di Roma <diromaalessandro@gmail.com>
2024-01-05 16:23:55 +0100

    Configurazione port e host

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
 import fastify from "fastify";
-import dbConnector from "./plugins/mysqlConnector";
 import controllers from "./api/controllers";
-import { Env } from "./env";
+import dbConnector from "./plugins/mysqlConnector";
+
+const port = Number(process.env.PORT) || 3000;
+const host = "RENDER" in process.env ? `0.0.0.0` : `localhost`;
 
 const server = fastify().withTypeProvider<TypeBoxTypeProvider>();
 
@@ -10,9 +12,9 @@ const server = fastify().withTypeProvider<TypeBoxTypeProvider>();
 server.register(dbConnector);
 
 // controllers
-server.register(controllers, {prefix: "/api/v1"});
+server.register(controllers, { prefix: "/api/v1" });
 
-server.listen({ port: 3000 }, (err, address) => {
+server.listen({ port, host }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
